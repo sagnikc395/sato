6c03076ce7efe784d943e8801fff734cf19cb4dc
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-08-11 23:36:14 +0530

    src: define a guess for a value

--- sato.ml
+++ sato.ml
@@ -14,5 +14,13 @@ let rec free_variable = function
       | Some _ as result -> result
       | None -> free_variable y
 
-      
+let rec guess_variable var var_bool expr = 
+  match expr with 
+  | Var v -> if v = var then Const val_bool else Var v 
+  | Not e -> Not (guess e)
+  | Or (x,y) -> Or(guess x, guess y)
+  | And (x,y) -> And (guess x , guess y)
+  | Const b -> Const b 
+  where guess = guess_variable var val_bool
+
 let () = print_endline "Hello, World!"
